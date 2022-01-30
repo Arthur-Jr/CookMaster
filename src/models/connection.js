6beddefe5da66e99aae1678e11be0f71c8cc2e31
Arthur-Jr <arthurjr1906@gmail.com>
2022-01-30 18:10:01 -0300

    Refatora a connection.

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/cookmaster';
+const MONGO_DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/cookmaster';
 const DB_NAME = 'cookmaster';
 
 module.exports = () => mongodb.connect(MONGO_DB_URL, {
